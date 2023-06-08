05cc94094b24097b54e7348745c0a158fad3eff9
Thomas Carrot <thomas.carrot38240@gmail.com>
2023-06-08 17:01:43 +0200

    base

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Start game
 /* ####################################################################### */
 /*                                START                                    */
 /* ####################################################################### */
@@ -8,19 +7,21 @@ kaboom({
     scale: 1,
     debug: true,
     clearColor: [0, 0, 0, 1],
-    background: [74, 48, 82],
+    background: [11, 17, 30],
 })
-
 /* ####################################################################### */
 /*                              Paramètre                                  */
 /* ####################################################################### */
-
-setGravity(500)
+setGravity(600)
 const SPEED = 300
 
 const ENEMY_SPEED = 40
+
+const BOSS_SPEED = 300
+const BOSS_HEALTH = 60
+
 const BULLET_SPEED = 500
-const JUMP_FORCE = 270
+const JUMP_FORCE = 300
 
 positionFire = 1;
 
@@ -28,7 +29,6 @@ const regex1 = '>';
 const regex2 = '@';
 let arrayPositionEnemies = [];
 let arrayPositionFlammesBar = [];
-
 /* ####################################################################### */
 /*                                 Blocs                                   */
 /* ####################################################################### */
@@ -44,11 +44,8 @@ loadSprite("vide", "ressources/blocs/vide.png")
 /* ####################################################################### */
 /*                                 MAP                                     */
 /* ####################################################################### */
-
 const LEVELS = [
     [
-        "                                                                                                                          ",
-        "                                                                                                                          ",
         "                                                                                                                          ",
         "                                                                                                                          ",
         "        a                                       z                                                                         ",
@@ -60,12 +57,12 @@ const LEVELS = [
         "                            $   =                                                                                         ",
         "                           ...  =                                                                                         ",
         "                                =        $                                                                                ",
-        "                 $              =      @...                                          $$$                                  ",
-        "                ...             =          .                                 @==================                          ",
-        "                                =                       >                                                                 ",
-        "                                =                                        $                                            /   ",
+        "                 $              =       ...                                          $$$                                  ",
+        "                ...             =          .                                  ============                                ",
+        "                                =                                                                   >                     ",
+        "                                =                        >               $                                            /   ",
         "                                =                                     ========                                       ///  ",
-        "======================================================================........============================================",
+        "======================================================================        ============================================",
         //"..........................................................................................................................",
     ],
     [
@@ -76,22 +73,18 @@ const LEVELS = [
         "                                                                                                                          ",
         "                                                                                                                          ",
         "                                                                                                                          ",
-        "                                                                                                                          ",
-        "                                                                                                                          ",
-        "                                                                                                                          ",
-        "                            $   =                                                                                         ",
-        "                           ...  =                                                                                         ",
-        "                                =        $                                                                                ",
-        "                 $              =       ...                       @                  $$$                                  ",
-        "                ...             =          .                                  ==================                          ",
-        "                                =                       >                                                                 ",
+        "                            $                               @                                                             ",
+        "                           ...                                                                                            ",
+        "                                         $                                                                                ",
+        "                 $                      ...                                          $$$                                  ",
+        "                ...                        .                                  =============                               ",
+        "                                =                                                                                         ",
         "                                =                                        $                                            /   ",
         "                                =                                     ========                                       ///  ",
-        "======================================================================........============================================",
+        "======================================================================        ============================================",
         //"..........................................................................................................................",
     ],
 ]
-
 scene("game", ({ levelIdx, score }) => {
 
     // Use the level passed, or first level
@@ -140,10 +133,10 @@ scene("game", ({ levelIdx, score }) => {
                 text('z for shoot'),
             ],
         },
-
     })
 
-    for (let j = 0; j < 18; j++) {
+    // Recherche de méchant ou flammesBarre dans la map
+    for (let j = 0; j <= 15; j++) {
         posMechant = (LEVELS[levelIdx][j].search(regex1))
         posFlammesBar = (LEVELS[levelIdx][j].search(regex2))
         //console.log(posMechant)
@@ -158,8 +151,8 @@ scene("game", ({ levelIdx, score }) => {
         if (posFlammesBar !== -1) {
             arrayPositionFlammesBar.push(
                 {
-                    "x": (100 + (posFlammesBar * 16)),
-                    "y":(345 + (j)),
+                    "x": (120 + (posFlammesBar * 16)),
+                    "y":(320 + j),
                     "deg": 60,
                     "num": 6,
                 }
@@ -167,66 +160,69 @@ scene("game", ({ levelIdx, score }) => {
         }
     }
 
+    // plein écran
     onKeyPress("f", () => {
         setFullscreen(!isFullscreen())
     })
-
-    posMechant = null;
-    //console.log(arrayPositionEnemies);
-
+    /* ####################################################################### */
+    /*                             Joueur                                      */
+    /* ####################################################################### */
+    // Ajouter un joueur
     player = add([
         sprite("mario"),
         area(),
         body(),
         pos(140,300),
     ])
-    // Get the player object from tag
 
-    player.onCollide("coin", (coin) => {
-        destroy(coin)
-        score++
-        scoreLabel.text = score
+    // camera
+    player.onUpdate(() => {
+        // Set the viewport center to player.pos
+        camPos(player.worldPos())
     })
+    player.onPhysicsResolve(() => {
+        // Set the viewport center to player.pos
+        camPos(player.worldPos())
+    })
+
     // Movements
     onKeyPress("space", () => {
         if (player.isGrounded()) {
             player.jump(JUMP_FORCE)
         }
     })
-
     onKeyDown("left", () => {
         player.move(-SPEED, 0)
         positionFire = 180;
     })
-
     onKeyDown("right", () => {
         player.move(SPEED, 0)
         positionFire = 1;
     })
+
     // Fall death
     player.onUpdate(() => {
         if (player.pos.y >= 500) {
             go("lose", { score: score })
         }
     })
-
-    player.onUpdate(() => {
-        // Set the viewport center to player.pos
-        camPos(player.worldPos())
-    })
-
-    player.onPhysicsResolve(() => {
-        // Set the viewport center to player.pos
-        camPos(player.worldPos())
-    })
-
-
+    // contact bullet
     player.onCollide("bullet", (bullet) => {
         destroy(bullet)
         destroy(player)
         go("lose", { score: score })
     })
-
+    // contact flamme
+    player.onCollide("flame", () => {
+        player.destroy()
+        go("lose", { score: score })
+    })
+    // collect de jeton
+    player.onCollide("coin", (coin) => {
+        destroy(coin)
+        score++
+        scoreLabel.text = score
+    })
     // Enter the next level on drapeau
     player.onCollide("drapeau", () => {
         if (levelIdx < LEVELS.length - 1) {
@@ -237,44 +233,38 @@ scene("game", ({ levelIdx, score }) => {
             })
         } else {
             // Otherwise we have reached the end of game, go to "win" scene!
-            go("win", { score: score })
+            go("battle", {
+                levelIdBoss: 0,
+                score: score,
+            })
         }
     })
 
-    player.onCollide("flame", () => {
-        player.destroy()
-        go("lose", { score: score })
-    })
-
+    // Tirer
     function spawnBullet() {
         add([
             rect(5, 5),
             area(),
             pos(player.pos.sub(0, -5)),
             anchor("center"),
-            color(127, 127, 255),
-            outline(4),
+            color(YELLOW),
             move(positionFire, BULLET_SPEED),
             offscreen({ destroy: true }),
             // strings here means a tag
             "feu",
         ])
     }
-
-
-    onCollide("feu", "sol", (feu) => {
-        destroy(feu)
-    })
-
-    onCollide("feu", "vide", (feu) => {
-        destroy(feu)
-    })
-
-
     onKeyPress("z", async () => {
         spawnBullet()
     })
 
+    // Si une bullet touche un bloc
+    onCollide("feu", "sol", (feu) => {
+        destroy(feu)
+    })
+    onCollide("feu", "vide", (feu) => {
+        destroy(feu)
+    })
     /* ####################################################################### */
     /*                           Flamme Barre                                  */
     /* ####################################################################### */
@@ -344,7 +334,7 @@ scene("game", ({ levelIdx, score }) => {
                         move(dir, BULLET_SPEED),
                         rect(5, 5),
                         area(),
-                        offscreen({destroy: true}),
+                        offscreen({ destroy: true }),
                         anchor("center"),
                         color(RED),
                         "bullet",
@@ -391,7 +381,195 @@ scene("game", ({ levelIdx, score }) => {
     ])
 })
 
+const BOSS = [
+    [
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".                                      .",
+        ".======================================.",
+    ],
+]
+scene("battle", ({levelIdBoss, score})=> {
+    // Use the level passed, or first level
+    const level = addLevel(BOSS[levelIdBoss || 0], {
+        tileWidth: 32,
+        tileHeight: 32,
+        pos: vec2(100, 100),
+        tiles: {
+            "=": () => [
+                sprite("sol"),
+                area(),
+                scale(4),
+                body({ isStatic: true }),
+                tile({ isObstacle: true }),
+                "sol",
+            ],
+            ".": () => [
+                sprite("vide"),
+                area(),
+                scale(4),
+                body({ isStatic: true }),
+                tile({ isObstacle: true }),
+                "vide",
+            ],
+        },
+    })
+    /* ########################################################################### */
+    /* ########################################################################### */
 
+    // Plein écran
+    onKeyPress("f", () => {
+        setFullscreen(!isFullscreen())
+    })
+
+    // affichage titre KILL THE BOSS
+    function late(t) {
+        let timer = 0
+        return {
+            add() {
+                this.hidden = true
+            },
+            update() {
+                timer += dt()
+                if (timer >= t) {
+                    this.hidden = false
+                }
+            },
+        }
+    }
+    add([
+        text("KILL", { size: 160 }),
+        pos(width() / 2, height() / 2),
+        anchor("center"),
+        lifespan(1),
+        fixed(),
+    ])
+    add([
+        text("THE", { size: 80 }),
+        pos(width() / 2, height() / 2),
+        anchor("center"),
+        lifespan(2),
+        late(1),
+        fixed(),
+    ])
+    add([
+        text("BOSS", { size: 120 }),
+        pos(width() / 2, height() / 2),
+        anchor("center"),
+        lifespan(4),
+        late(2),
+        fixed(),
+    ])
+    /* ########                ######                ###### */
+
+    /* ########                PLAYER                ###### */
+    const player = add([
+        sprite("mario"),
+        area(),
+        body(),
+        scale(2),
+        pos(140,300),
+    ])
+    // déplacement
+    onKeyDown("left", () => {
+        player.move(-SPEED, 0)
+        if (player.pos.x < 0) {
+            player.pos.x = width()
+        }
+    })
+    onKeyDown("right", () => {
+        player.move(SPEED, 0)
+        if (player.pos.x > width()) {
+            player.pos.x = 0
+        }
+    })
+    // tirer
+    function spawnBullet(p) {
+        add([
+            rect(12, 48),
+            area(),
+            pos(p),
+            anchor("center"),
+            color(127, 127, 255),
+            outline(4),
+            move(UP, BULLET_SPEED),
+            offscreen({ destroy: true }),
+            "bullet",
+        ])
+    }
+    onKeyPress("z", () => {
+        spawnBullet(player.pos.sub(-20, 10))
+    })
+
+    /* ########                BOSS                ###### */
+    // création
+    const boss = add([
+        sprite("en"),
+        area(),
+        pos(width() / 2, 40),
+        health(BOSS_HEALTH),
+        scale(5),
+        anchor("top"),
+        "enemy",
+        {
+            dir: 1,
+        },
+    ])
+    // déplacement
+    boss.onUpdate( () => {
+        boss.move(BOSS_SPEED * boss.dir, 0)
+        if (boss.dir === 1 && boss.pos.x >= width() - 250) {
+            boss.dir = -1
+        }
+        if (boss.dir === -1 && boss.pos.x <= 200) {
+            boss.dir = 1
+        }
+    })
+    // boss toucher
+    onCollide("bullet", "enemy", (b, e) => {
+        destroy(b)
+        e.hurt(1)
+    })
+    boss.onHurt(() => {
+        healthbar.set(boss.hp())
+    })
+    // boss mort
+    boss.onDeath(() => {
+        go("win", { score: (score) })
+    })
+    // barre de vie
+    const healthbar = add([
+        rect(width(), 24),
+        pos(0, 0),
+        color(107, 201, 108),
+        fixed(),
+        {
+            max: BOSS_HEALTH,
+            set(hp) {
+                this.width = width() * hp / this.max
+            },
+        },
+    ])
+    /* ########################################################################### */
+    /* ########################################################################### */
+})
 
 
 /* ########################################################################### */
@@ -439,13 +617,19 @@ scene("win", ({ score }) => {
 
 function start() {
     // Start with the "game" scene, with initial parameters
+    /*
+    go("battle", {
+        levelIdBoss: 0,
+        score: 0,
+    })
+
+     */
     go("game", {
         levelIdx: 0,
         score: 0,
     })
 }
 start()
-
 /* ################################################################################################################## */
 /* ################################################################################################################## */
 /* ################################################################################################################## */
